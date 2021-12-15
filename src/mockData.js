9282774b2908fcce9486436ce6ad8220f63aa7bd
Ryan <ryanzhou7@gmail.com>
2021-12-14 19:25:57 -0800

    mock data

---
+++ src/mockData.js
@@ -0,0 +1,37 @@
+const data = {
+  "North America": {
+    headers: ["Column name", "Type", "Description"],
+    rows: [
+      {
+        uuid: 1,
+        data: ["Profit", "INT", "..."],
+      },
+      {
+        uuid: 2,
+        data: ["Loss", "INT", "..."],
+      },
+    ],
+  },
+  "South America": {
+    headers: ["Column name", "Type", "Description"],
+    rows: [
+      {
+        uuid: 3,
+        data: ["Income", "INT", "..."],
+      },
+      {
+        uuid: 4,
+        data: ["Deductions", "INT", "..."],
+      },
+      {
+        uuid: 5,
+        data: ["First Name", "VARCHAR(5)", "..."],
+      },
+      {
+        uuid: 6,
+        data: ["Last Name", "VARCHAR(5)", "..."],
+      },
+    ],
+  },
+};
+export default data;
